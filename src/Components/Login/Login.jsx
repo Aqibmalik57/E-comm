c36d6760f3fcecafa1f4874fb0c73a8def9a4cb8
Aqibmalik57 <aqibmalik1586@gmail.com>
2024-10-28 13:05:55 +0500

    login change

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { login } from "../../store/feature/userSlice";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../../store/feature/userSlice';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const [isLoginAttempted, setIsLoginAttempted] = useState(false);
-  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+  const [showPassword, setShowPassword] = useState(false);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -20,30 +20,30 @@ const Login = () => {
   const validateEmail = (value) => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!value) {
-      setEmailError("Email is required");
+      setEmailError('Email is required');
     } else if (!emailPattern.test(value)) {
-      setEmailError("Please enter a valid email address");
+      setEmailError('Please enter a valid email address');
     } else {
-      setEmailError("");
+      setEmailError('');
     }
   };
 
   const validatePassword = (value) => {
     if (!value) {
-      setPasswordError("Password is required");
+      setPasswordError('Password is required');
     } else if (value.length < 8) {
-      setPasswordError("Password must be at least 8 characters long");
+      setPasswordError('Password must be at least 8 characters long');
     } else {
-      setPasswordError("");
+      setPasswordError('');
     }
   };
 
   useEffect(() => {
     if (user && isLoginAttempted) {
-      if (user.role === "admin") {
-        navigate("/dashboard");
+      if (user.role === 'admin') {
+        navigate('/dashboard');
       } else {
-        navigate("/home");
+        navigate('/home');
       }
     }
   }, [user]);
@@ -61,104 +61,104 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className='min-h-screen flex items-center justify-center bg-white'>
       {loading ? (
-        <div className="flex justify-center items-center">Loading...</div>
+        <div className='flex justify-center items-center'>Loading...</div>
       ) : (
-        <div className="bg-gray-50 p-10 rounded-lg shadow-xl w-full max-w-md">
-          <h2 className="text-3xl font-bold mb-8 text-center text-[#10b981]">
+        <div className='bg-gray-50 p-10 rounded-lg shadow-xl w-full max-w-md'>
+          <h2 className='text-3xl font-bold mb-8 text-center text-[#10b981]'>
             Login
           </h2>
           <form onSubmit={handleLogin}>
-            <div className="mb-6">
+            <div className='mb-6'>
               <label
-                className="block text-gray-700 text-sm font-semibold mb-2"
-                htmlFor="email"
+                className='block text-gray-700 text-sm font-semibold mb-2'
+                htmlFor='email'
               >
                 Email
               </label>
               <input
-                type="email"
-                id="email"
-                placeholder="Enter your email"
+                type='email'
+                id='email'
+                placeholder='Enter your email'
                 value={email}
                 onChange={(e) => {
                   setEmail(e.target.value);
                   validateEmail(e.target.value);
                 }}
                 className={`w-full p-3 border rounded-md focus:outline-none focus:ring-2 ${
-                  emailError ? "border-red-500" : "border-gray-300"
+                  emailError ? 'border-red-500' : 'border-gray-300'
                 } focus:ring-[#10b981]`}
                 required
               />
               {emailError && (
-                <div className="text-red-500 text-sm">{emailError}</div>
+                <div className='text-red-500 text-sm'>{emailError}</div>
               )}
             </div>
 
-            <div className="mb-6 relative">
+            <div className='mb-6 relative'>
               <label
-                className="block text-gray-700 text-sm font-semibold mb-2"
-                htmlFor="password"
+                className='block text-gray-700 text-sm font-semibold mb-2'
+                htmlFor='password'
               >
                 Password
               </label>
               <input
-                type={showPassword ? "text" : "password"} 
-                id="password"
-                placeholder="Enter your password"
+                type={showPassword ? 'text' : 'password'}
+                id='password'
+                placeholder='Enter your password'
                 value={password}
                 onChange={(e) => {
                   setPassword(e.target.value);
                   validatePassword(e.target.value);
                 }}
                 className={`w-full p-3 border rounded-md focus:outline-none focus:ring-2 ${
-                  passwordError ? "border-red-500" : "border-gray-300"
+                  passwordError ? 'border-red-500' : 'border-gray-300'
                 } focus:ring-[#10b981]`}
                 required
               />
               <div
-                className="absolute top-11 right-0 pr-4 flex items-center cursor-pointer"
+                className='absolute top-11 right-0 pr-4 flex items-center cursor-pointer'
                 onClick={() => setShowPassword(!showPassword)}
               >
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </div>
               {passwordError && (
-                <div className="text-red-500 text-sm">{passwordError}</div>
+                <div className='text-red-500 text-sm'>{passwordError}</div>
               )}
             </div>
 
-            <div className="flex justify-between items-center mb-6">
-              <div className="flex items-center">
+            <div className='flex justify-between items-center mb-6'>
+              <div className='flex items-center'>
                 <input
-                  type="checkbox"
-                  id="rememberMe"
-                  className="mr-2 accent-[#10b981]"
+                  type='checkbox'
+                  id='rememberMe'
+                  className='mr-2 accent-[#10b981]'
                 />
                 <label
-                  htmlFor="rememberMe"
-                  className="text-gray-700 text-sm font-semibold"
+                  htmlFor='rememberMe'
+                  className='text-gray-700 text-sm font-semibold'
                 >
                   Remember Me
                 </label>
               </div>
               <div>
-                <Link to="/forgetpassword" className="text-[#10b981] text-sm">
+                <Link to='/forgetpassword' className='text-[#10b981] text-sm'>
                   Forgot Password?
                 </Link>
               </div>
             </div>
 
-            <div className="flex justify-center">
+            <div className='flex justify-center'>
               <button
-                type="submit"
-                className="bg-[#10b981] text-white py-2 px-4 rounded-md hover:bg-green-700 transition duration-200"
+                type='submit'
+                className='bg-[#10b981] text-white py-2 px-4 rounded-md hover:bg-green-700 transition duration-200'
               >
                 Login
               </button>
             </div>
-            <div className="mt-4 text-center">
-              <Link to="/signup" className="text-[#10b981]">
+            <div className='mt-4 text-center'>
+              <Link to='/signup' className='text-[#10b981]'>
                 Don't have an account? Sign Up
               </Link>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Imageprovider from "./Components/Context/imagesContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Imageprovider from './Components/Context/imagesContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Provider store={store}>
-        <Imageprovider>
-          <App />
-          <ToastContainer position="top-center" />
-        </Imageprovider>
+      <Imageprovider>
+        <App />
+        <ToastContainer position='top-center' />
+      </Imageprovider>
     </Provider>
   </>
 );
